712f37f27e66663e5087ac14f7c82db25603d0cb
Alexey Serdiuk <ytfrpkkpreim@mail.ru>
2025-10-05 03:38:46 +0300

    Add currency filtering functionality

---
+++ src/generators.py
@@ -0,0 +1,30 @@
+def filter_by_currency(transactions, currency_code):
+    """
+    Фильтрует транзакции по заданному коду валюты.
+    Функция получает список транзакций, затем выбирает только те транзакции,
+    в которых валюта совпадает либо по коду, либо по имени.
+
+    Args:
+    transactions (list[dict]): Список словарей, содержащих информацию о транзакциях.
+    currency_code (str): Строка, содержащая код валюты для фильтрации (например, "USD").
+
+    Returns:
+    Iterable[dict]: Итератор, выдающий транзакции, соответствующие заданной валюте.
+    """
+
+    # Фильтрация транзакций, имеющих ключ 'operationAmount'.
+    operations_amount = filter(lambda t: "operationAmount" in t, transactions)
+
+    # Выбор транзакций, содержащих информацию о валюте ('currency')
+    currency_info = filter(lambda t: "currency" in t["operationAmount"], operations_amount)
+
+    # Поиск транзакций, где код или название валюты совпадают с искомым значением.
+    matching_currency = filter(
+        lambda t: (
+            t["operationAmount"]["currency"].get("code") == currency_code
+            or t["operationAmount"]["currency"].get("name") == currency_code
+        ),
+        currency_info,
+    )
+
+    return matching_currency
